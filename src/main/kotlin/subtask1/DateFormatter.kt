8b8453d1addf0e61ca828070accbf7c7c810f16f
karachun.dmitry@mail.ru <1282s8u6>
2020-04-13 13:05:03 +0300

    FEAT: add solution task_1

--- src/main/kotlin/subtask1/DateFormatter.kt
+++ src/main/kotlin/subtask1/DateFormatter.kt
@@ -1,9 +1,25 @@
 package subtask1
 
+import java.time.LocalDate
+import java.time.YearMonth
+import java.time.format.TextStyle
+import java.util.*
+
 class DateFormatter {
 
-    // TODO: Complete the following function
     fun toTextDay(day: String, month: String, year: String): String {
-        throw NotImplementedError("Not implemented")
+        val yearToInt = year.toInt();
+        val dayToInt = day.toInt();
+        val monthToInt = month.toInt();
+        if (monthToInt > 12 || dayToInt > YearMonth.of(yearToInt, monthToInt).lengthOfMonth()) {
+            return "Такого дня не существует";
+        }
+        val date = LocalDate.of(yearToInt, monthToInt, dayToInt);
+        val dayOfWeek = date.dayOfWeek.getDisplayName(TextStyle.FULL, Locale("ru"));
+        val monthValue = date.month.getDisplayName(TextStyle.FULL, Locale("ru"));
+        return "$day $monthValue, $dayOfWeek";
     }
 }
+
+
+

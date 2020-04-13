bedd3e55d5d56873eceeb99c725fd34553530bbf
karachun.dmitry@mail.ru <1282s8u6>
2020-04-13 13:38:59 +0300

    FEAT: add solution task_2

--- src/main/kotlin/subtask2/TimeConverter.kt
+++ src/main/kotlin/subtask2/TimeConverter.kt
@@ -2,8 +2,63 @@ package subtask2
 
 class TimeConverter {
 
-    // TODO: Complete the following function
     fun toTextFormat(hour: String, minute: String): String {
-        throw NotImplementedError("Not implemented")
+        val arrayOfNumbers = arrayOf(
+            "one",
+            "two",
+            "three",
+            "four",
+            "five",
+            "six",
+            "seven",
+            "eight",
+            "nine",
+            "ten",
+            "eleven",
+            "twelve",
+            "thirteen",
+            "fourteen",
+            "fifteen",
+            "sixteen",
+            "seventeen",
+            "eighteen",
+            "nineteen",
+            "twenty",
+            "twenty one",
+            "twenty two",
+            "twenty three",
+            "twenty four",
+            "twenty five",
+            "twenty six",
+            "twenty seven",
+            "twenty eight",
+            "twenty nine"
+        );
+        val hourToInt = Integer.parseInt(hour);
+        val minuteToInt = Integer.parseInt(minute);
+        if (hourToInt > 12) {
+            return "";
+        }
+        return when (minuteToInt) {
+            0 -> "${arrayOfNumbers[hourToInt - 1]} o' clock"
+            in (1..29).filter { it != 15 } -> {
+                if (minuteToInt == 1) {
+                    "one minute past ${arrayOfNumbers[hourToInt - 1]}";
+                } else {
+                    "${arrayOfNumbers[minuteToInt - 1]} minutes past ${arrayOfNumbers[hourToInt - 1]}";
+                }
+            }
+            15 -> "quarter past ${arrayOfNumbers[hourToInt - 1]}";
+            30 -> "half past ${arrayOfNumbers[hourToInt - 1]}";
+            45 -> "quarter to ${arrayOfNumbers[hourToInt]}";
+            in (31..59).filter { it != 45 } -> {
+                if (minuteToInt == 31) {
+                    "one minute to ${arrayOfNumbers[hourToInt - 1]}";
+                } else {
+                    "${arrayOfNumbers[59 - minuteToInt]} minutes to ${arrayOfNumbers[hourToInt]}";
+                }
+            }
+            else -> "";
+        }
     }
 }
